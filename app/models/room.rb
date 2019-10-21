6eea86b970dbac29ca816f9b2d986b040814049e
Muthukumar Rajendran <kumar_muthu@hotmail.com>
2019-10-22 01:25:29 +0530

    connected room mixed room-type

--- app/models/room.rb
+++ app/models/room.rb
@@ -11,29 +11,59 @@ class Room < ApplicationRecord
     san_dtstart = connection.quote(dtstart.strftime('%m/%d/%Y'))
     san_dtsend = connection.quote(dtend.strftime('%m/%d/%Y'))
     bookings = connection.execute("
-              WITH current_room_bookings AS (
-                 SELECT *
-                 FROM bookings
-                 WHERE bookings.room_id = #{self.id}
-                   AND bookings.dtstart >= #{san_dtstart} and bookings.dtend < #{san_dtsend}
-                 ),
+          -- Get all booking across rooms in the given range
+            WITH current_room_bookings AS
+          ( SELECT *
+            FROM bookings
+            WHERE (bookings.dtstart >= #{san_dtstart}  AND bookings.dtstart < #{san_dtsend})
+             OR (bookings.dtend > #{san_dtstart}  AND bookings.dtend <= #{san_dtsend})
+           ),
 
-                 current_room_blocks AS
-                (SELECT DISTINCT a.room_unit_id,
-                                 c.id
-                 FROM current_room_bookings A
-                 INNER JOIN room_units B ON a.room_unit_id = b.id
-                 LEFT JOIN room_units C ON c.id = b.part_of_room_id
-                 OR c.part_of_room_id = b.id)
+          -- Get room unit connection information
+           connected_rooms AS
+          (SELECT DISTINCT b.id primary_unit_id,
+                           b.room_id primary_room_id,
+                           c.id connected_unit_id,
+                           c.room_id connected_room_id
+           FROM current_room_bookings A
+           INNER JOIN room_units B ON a.room_unit_id = b.id
+           LEFT JOIN room_units C ON c.id = b.part_of_room_id
+           OR c.part_of_room_id = b.id
+           ),
 
-              SELECT series,
-                     Count(DISTINCT bookings.room_unit_id) + Count(DISTINCT blocks.id) as count
-              FROM generate_series(#{san_dtstart},#{san_dtsend},interval '1 day') series
-              LEFT JOIN current_room_bookings bookings ON series >= bookings.dtstart
-              AND series < bookings.dtend
-              LEFT JOIN current_room_blocks blocks ON bookings.room_unit_id = blocks.room_unit_id
-              GROUP BY series
-              ORDER BY series").to_a
+          -- Connect Bookings => Room Units => Connected Room Units
+           combined1 AS
+          (SELECT series,
+                  bookings.room_id,
+                  blocks.*
+           FROM generate_series(#{san_dtstart}, #{san_dtsend},interval '1 day') series
+           LEFT JOIN current_room_bookings bookings ON series >= bookings.dtstart
+           AND series < bookings.dtend
+           LEFT JOIN connected_rooms blocks ON bookings.room_unit_id IN (blocks.primary_unit_id,
+                                                                         blocks.connected_unit_id)
+           AND bookings.room_id IN(blocks.primary_room_id,
+                                   blocks.primary_room_id)),
+
+          -- Filter the current room
+           combined2 AS
+          (
+           SELECT series, primary_unit_id AS book
+           FROM combined1 WHERE primary_room_id =  #{self.id}
+           UNION
+           SELECT series, connected_unit_id
+           FROM combined1 WHERE connected_room_id =  #{self.id}
+           UNION
+           SELECT distinct series, null::bigint
+           FROM combined1
+           )
+
+          -- Get count of booking for each date
+          SELECT series,
+                 count(book)
+          FROM combined2
+          GROUP BY series
+          ORDER BY series").to_a
+
     total_rooms = self.room_units.count
 
     payload = bookings.map do |booking|
